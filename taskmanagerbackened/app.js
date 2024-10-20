98c5219b4423109c32f6075c5395d00308c80dbc
Mamata Singh <mamata.singh@privado.ai>
2024-10-20 18:55:03 +0530

    mongodb seeting for production

--- taskmanagerbackened/app.js
+++ taskmanagerbackened/app.js
@@ -9,10 +9,8 @@ const categoryRoutes = require('./routes/categories');
 const app = express();
 app.use(express.json());
 app.use(cors());
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log(err));
-
+const connectDB = require('./config/database');
+connectDB();
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
